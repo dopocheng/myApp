68c5c133bfab99d42c18438078603cde0b8933d0
dopocheng <747015697@qq.com>
2018-03-10 13:03:01 +0800

    ionic3_myApp
    下载ionic项目，修改tabs图标和对应页面，删除不需要的页面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ChatPage } from '../pages/chat/chat';
+import { NotificationPage } from '../pages/notification/notification';
+import { MorePage } from '../pages/more/more';
+import { DiscoveryPage } from '../pages/discovery/discovery';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
     TabsPage
   ],
   imports: [
@@ -26,9 +30,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    DiscoveryPage,
+    ChatPage,
+    NotificationPage,
+    MorePage,
     TabsPage
   ],
   providers: [
