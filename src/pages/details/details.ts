3b3f81cb5f9c5c5f6720d073223c3e846bc5dae5
dopocheng <7474015697@qq.com>
2018-04-01 00:41:14 +0800

    完善 问题详细页面(2018-4-1)
    1.将页面测试数据换成后台真实返回的数据；
    2.通过向后台传递两个 id 处理返回状态，前端判断直接展示
    3.灰掉自己的提问，两个 id 是否相等，前台处理技巧[disabled]="isMyQuestion"
    4.{{question?.Content}}处理question为空对象的方法

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
+
+import { BaseUI } from '../../common/baseui';
+import { RestProvider } from '../../providers/rest/rest';
+import { Storage } from '@ionic/storage'
 
 /**
  * Generated class for the DetailsPage page.
@@ -8,18 +12,73 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
+// @IonicPage()
 @Component({
   selector: 'page-details',
   templateUrl: 'details.html',
 })
-export class DetailsPage {
+export class DetailsPage extends BaseUI {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  id: string;//前一个页面的 id
+  userId: string;//当前使用者 id
+  question: string[];//问题列表
+  answers: string[];//问题的回答列表
+  errorMessage: any;//错误处理
+  isFavourite: boolean;//是否关注
+  isMyQuestion: boolean;//是否自己的问题
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public rest: RestProvider,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController,
+    public storage: Storage
+  ) {
+    super();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DetailsPage');
+    //获得页面传过来的参数
+    this.id = this.navParams.get('id');
+    this.loadQuestion(this.id);
+  }
+  //加载问题内容和回答的内容
+  loadQuestion(id) {
+    this.storage.get('UserId').then(
+      (val) => {
+        if (val !== null) {
+          this.userId = val;
+          var loading = super.showLoading(this.loadingCtrl, "数据加载中。。。");
+          this.rest.getQuestionWithUser(id, val)
+            .subscribe(
+              q => {
+                this.question = q;
+                //对问题的回答
+                this.answers = q["Answers"];
+                this.isFavourite = q["IsFavourite"];
+                //提问的 id 与当前的 id 比较是否相等
+                this.isMyQuestion = (q["OwnUserId"] == val);
+              }, 
+              error => this.errorMessage = <any>error);
+        }
+      }
+    );
+  }
+  //关注和取消关注
+  saveFavourte() {
+    console.log("saveFavourte");
+    var loading = super.showLoading(this.loadingCtrl, "请求中。。。");
+    this.rest.saveFavourte(this.id, this.userId)
+      .subscribe(
+        f => {
+          //两个 id 通过后台判断， 返回一个状态 
+          if (f["Status"] == "OK") {
+            loading.dismiss();
+            super.showToast(this.toastCtrl, this.isFavourite ? "取消关注成功!" : "关注问题成功!");
+            this.isFavourite = !this.isFavourite;
+          }
+        },
+        error => this.errorMessage = <any>error);
   }
 
 }
--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -1,10 +1,10 @@
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 
-import { Http,Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
- 
+
 /*
   Generated class for the RestProvider provider.
 
@@ -18,27 +18,27 @@ export class RestProvider {
     //console.log('Hello RestProvider Provider');
   }
 
-    //feed
-    private apiUrlFeeds = 'https://imoocqa.gugujiankong.com/api/feeds/get';
+  //feed
+  private apiUrlFeeds = 'https://imoocqa.gugujiankong.com/api/feeds/get';
 
-    //account
-    private apiUrlRegister = 'https://imoocqa.gugujiankong.com/api/account/register';
-    private apiUrlLogin = 'https://imoocqa.gugujiankong.com/api/account/login';
-    private apiUrlUserInfo = 'https://imoocqa.gugujiankong.com/api/account/userinfo';
-    private apiUrlUpdateNickName = 'https://imoocqa.gugujiankong.com/api/account/updatenickname';
-    private apiGetUserQuestionList = "https://imoocqa.gugujiankong.com/api/account/getuserquestionlist";
-  
-    //question
-    private apiUrlQuestionSave = 'https://imoocqa.gugujiankong.com/api/question/save';
-    private apiUrlQuestionList = 'https://imoocqa.gugujiankong.com/api/question/list?index=1&number=10';
-    private apiUrlGetQuestion = "https://imoocqa.gugujiankong.com/api/question/get";
-    private apiUrlGetQuestionWithUser = "https://imoocqa.gugujiankong.com/api/question/getwithuser";
-    private apiUrlAnswer = "https://imoocqa.gugujiankong.com/api/question/answer";
-    private apiUrlSaveFavourite = "https://imoocqa.gugujiankong.com/api/question/savefavourite";
-  
-    //notification
-    private apiUrlUserNotifications = "https://imoocqa.gugujiankong.com/api/account/usernotifications";
-  
+  //account
+  private apiUrlRegister = 'https://imoocqa.gugujiankong.com/api/account/register';
+  private apiUrlLogin = 'https://imoocqa.gugujiankong.com/api/account/login';
+  private apiUrlUserInfo = 'https://imoocqa.gugujiankong.com/api/account/userinfo';
+  private apiUrlUpdateNickName = 'https://imoocqa.gugujiankong.com/api/account/updatenickname';
+  private apiGetUserQuestionList = "https://imoocqa.gugujiankong.com/api/account/getuserquestionlist";
+
+  //question
+  private apiUrlQuestionSave = 'https://imoocqa.gugujiankong.com/api/question/save';
+  private apiUrlQuestionList = 'https://imoocqa.gugujiankong.com/api/question/list?index=1&number=10';
+  private apiUrlGetQuestion = "https://imoocqa.gugujiankong.com/api/question/get";
+  private apiUrlGetQuestionWithUser = "https://imoocqa.gugujiankong.com/api/question/getwithuser";
+  private apiUrlAnswer = "https://imoocqa.gugujiankong.com/api/question/answer";
+  private apiUrlSaveFavourite = "https://imoocqa.gugujiankong.com/api/question/savefavourite";
+
+  //notification
+  private apiUrlUserNotifications = "https://imoocqa.gugujiankong.com/api/account/usernotifications";
+
   /**
    * 根据用户手机号码和密码进行登录
    * 
@@ -48,30 +48,62 @@ export class RestProvider {
    * @memberof RestProvider
    */
 
-  login(mobile,password): Observable<string[]>{
-    return this.getUrlReturn(this.apiUrlLogin+ "?mobile=" + mobile + "&password=" + password);
+  login(mobile, password): Observable<string[]> {
+    return this.getUrlReturn(this.apiUrlLogin + "?mobile=" + mobile + "&password=" + password);
   }
 
   //获取用户基本信息
   //报错userinfo not function ("?userId=")少了=
   getUserInfo(userId): Observable<string[]> {
-    return this.getUrlReturn(this.apiUrlUserInfo +"?userId=" + userId);
-  } 
+    return this.getUrlReturn(this.apiUrlUserInfo + "?userId=" + userId);
+  }
 
   //请求保存api
-  updateNickName(userid,usernickname): Observable<string[]> {
+  updateNickName(userid, usernickname): Observable<string[]> {
     return this.getUrlReturn(this.apiUrlUpdateNickName + "?userId=" + userid + "&nickname=" + usernickname);
   }
   //保存问题
- saveQuestion(userId,title,content):Observable<string[]> {
-  return this.getUrlReturn(this.apiUrlQuestionSave + "?userId=" +userId + "&title=" + title + "&content=" + content);
- }
-
+  saveQuestion(userId, title, content): Observable<string[]> {
+    return this.getUrlReturn(this.apiUrlQuestionSave + "?userId=" + userId + "&title=" + title + "&content=" + content);
+  }
+  
   //获得首页的 feed 流
- getFeeds():Observable<string[]> {
-   return this.getUrlReturn(this.apiUrlFeeds);
- }
-
+  getFeeds(): Observable<string[]> {
+    return this.getUrlReturn(this.apiUrlFeeds);
+  }
+  /**
+   * 获取问题的详情
+   * 
+   * @param {any} id 
+   * @returns {Observable<string[]>} 
+   * @memberof RestProvider
+   */
+  getQuestion(id): Observable<string[]> {
+    return this.getUrlReturn(this.apiUrlGetQuestion + "?id=" + id);
+  }
+  /**
+   * 获取问题的详情,传递 userid 获取到当前用户有没有关注此问题
+   * 
+   * @param {any} questionId 
+   * @param {any} userId 
+   * @returns {Observable<string[]>} 
+   * @memberof RestProvider
+   */
+  getQuestionWithUser(questionId, userId): Observable<string[]> {
+    return this.getUrlReturn(this.apiUrlGetQuestionWithUser + "?id=" + questionId + "&userid=" + userId);
+  }
+  /**
+   * 向后台传递提问者的 id 和当前用户 id ，后台处理向前台返回 IsFavourite 状态；
+   * 页面三目运算展示是否关注。
+   * 
+   * @param {any} questionId 
+   * @param {any} userId 
+   * @returns {Observable<string[]>} 
+   * @memberof RestProvider
+   */
+  saveFavourte(questionId, userId): Observable<string[]> {
+    return this.getUrlReturn(this.apiUrlSaveFavourite + "?questionid=" + questionId + "&userid=" + userId);
+  }
   /**
    * 注册请求
    * 
@@ -81,10 +113,10 @@ export class RestProvider {
    * @returns {Observable<string[]>} 
    * @memberof RestProvider
    */
-  register(mobile,nickname,password): Observable<string[]> {
-    return this.getUrlReturn(this.apiUrlRegister+ "?mobile=" + mobile + "&nickname=" + nickname +"&password=" +password)
+  register(mobile, nickname, password): Observable<string[]> {
+    return this.getUrlReturn(this.apiUrlRegister + "?mobile=" + mobile + "&nickname=" + nickname + "&password=" + password)
   }
-  
+
 
   /**
    * 全局获取 HTTP 请求的方法
@@ -94,10 +126,10 @@ export class RestProvider {
    * @returns {Observable<string[]>} 返回类型
    * @memberof RestProvider 属于RestProvider
    */
-  private getUrlReturn(url:string):Observable<string[]>{//不理解
+  private getUrlReturn(url: string): Observable<string[]> {//不理解
     return this.http.get(url)
-                    .map(this.extractData)
-                    .catch(this.handleError);
+      .map(this.extractData)
+      .catch(this.handleError);
   }
 
   /**
@@ -108,11 +140,11 @@ export class RestProvider {
    * @returns 
    * @memberof RestProvider
    */
-  private extractData(res:Response){
+  private extractData(res: Response) {
     let body = res.json();
     return JSON.parse(body) || {};
   }
-  
+
   /**
    * 处理请求中的错误，考虑了各种情况的错误处理并在 console 中显示 error
    * 
@@ -121,16 +153,16 @@ export class RestProvider {
    * @returns 
    * @memberof RestProvider
    */
-  private handleError(error:Response | any){
-    let errMsg:string;
-    if(error instanceof Response){
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
       const body = error.json() || '';
       const err = body.error || JSON.stringify(body);
       errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    }else{
+    } else {
       errMsg = error.message ? error.message : error.toString();
     }
     console.error(errMsg);
-  return Observable.throw(errMsg);
+    return Observable.throw(errMsg);
   }
 }
