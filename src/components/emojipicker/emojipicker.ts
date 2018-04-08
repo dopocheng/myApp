21bb20d1577421a104669b4de9f2d4e0d65a0837
dopocheng <7474015697@qq.com>
2018-04-08 23:05:00 +0800

    将表情做成一个组件
    emoji 将图片分组
    emojipicker 做成组件供使用

--- src/components/emojipicker/emojipicker.ts
+++ src/components/emojipicker/emojipicker.ts
@@ -3,13 +3,16 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { EmojiProvider } from '../../providers/emoji/emoji';
 
 /**
- * Generated class for the EmojipickerComponent component.
+ *可以查看 Component API
  *
  * See https://angular.io/api/core/Component for more info on Angular
  * Components.
  */
+
+ //默认的定义做成自身的 provide
 export const EMOJI_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
+  // 传递给使用
   useExisting: forwardRef(() => EmojipickerComponent),
   multi: true
 }
@@ -17,28 +20,31 @@ export const EMOJI_ACCESSOR: any = {
 @Component({
   selector: 'emojipicker',
   templateUrl: 'emojipicker.html',
-  providers: [EMOJI_ACCESSOR],
+  providers: [EMOJI_ACCESSOR]
 })
 
 //自定义表单必须实现 控件 ControlValueAccessor 接口中的所有方法
 export class EmojipickerComponent implements ControlValueAccessor {
-
+  
   emojiArray = [];//存放 13 组 emoji 图标
   content: string;//输入框的内容
-  onChanged: Function;//要实现的方法
-  onTouch: Function;//要实现的方法
+  onChanged: any = () => { };//要实现的方法
+  onTouch: any = () => { };//要实现的方法
 
   constructor(emojiProvider: EmojiProvider) {
     this.emojiArray = emojiProvider.getEmojis();
   }
 
+  //写一个新的值到 element
   writeValue(obj: any): void {
-    this.emojiArray = obj;
+    // 判断 obj 是否是非 undefine null 空字符串，是则赋为空 
+    this.content = obj ? obj : "";
   }
   //registerOnChange() 可以用来通知外部，组件已经发生变化
   registerOnChange(fn: any): void {
     this.onChanged = fn;
-    this.setValue(this.onChanged);
+    console.log("registerOnChange");
+    this.setValue(this.content);
   }
 
   registerOnTouched(fn: any): void {
@@ -49,11 +55,12 @@ export class EmojipickerComponent implements ControlValueAccessor {
   //   throw new Error("Method not implemented.");
   // }
 
-  // click 事件传入 item 即 emoji 图标
-  setValue(val: any) {
+  // click 事件传入 item 即 emoji 图标再；处理新的赋值以及函数绑定
+  setValue(val: any): any{
+    // console.error("setValue");
     this.content += val;
     if (this.content) {
       this.onChanged(this.content);
     }
   }
-}
+};
